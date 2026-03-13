09b35241ae514dffec397117a892ada7a4660452
Peace Williams-Ojomu <peaceojomu@gmail.com>
2026-03-13 15:06:21 +0000

    schema load fix

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,6 +1,9 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const typesArray = loadFilesSync(path.join(__dirname, './typeDefs'), {
   extensions: ['graphql'],
